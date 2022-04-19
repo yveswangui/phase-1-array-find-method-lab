e1f5c69c4d36a646f9f5d5aed86f608df0327081
Yvonne <yveswangui@gmail.com>
2022-04-19 18:33:56 +0300

    commit tree ec7bd106f5a202306c93ff842fe981f6a4018a1f

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// code your solution here
+function superbowlWin(record){
+   for(const elem in record){
+       if (elem.result === 'W'){
+           return elem.year
+       } else {
+           return undefined
+       }
+   }
+}
+recArray.find(superbowlWin(record))
